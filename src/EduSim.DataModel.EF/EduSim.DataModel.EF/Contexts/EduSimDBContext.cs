3c0c2fd73adff44d1ef72373a8198fc927e6fad5
Andrew Clawson <andrewclawson@Andrews-MacBook-Pro.local>
2016-08-28 00:46:16 -0600

    add connection strings to App.config for entity framework. add context classes.

---
+++ src/EduSim.DataModel.EF/EduSim.DataModel.EF/Contexts/EduSimDBContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Data.Linq.Mapping;
+using MySql.Data.Entity;
+
+
+namespace EduSim.Database
+{
+	public class EduSimDBContext : DbContext, IApplicationDBContext
+	{
+		public EduSimDBContext() : base(nameOrConnectionString: "DevelopDBContext") { }
+		//public DbSet<Account> AccountList { get; set; }
+		//public DbSet<School> SchoolList { get; set; }
+		//public DbSet<User> UserList { get; set; }
+
+		/*
+		public void MarkAsModified(Account account)
+		{
+			Entry(account).State = EntityState.Modified;
+		}
+		*/
+
+		//Overloaded for User type.
+		//public void MarkAsModified(User user)
+		//{
+		//	Entry(user).State = EntityState.Modified;
+		//}
+
+		public override System.Threading.Tasks.Task<int> SaveChangesAsync()
+		{
+			return base.SaveChangesAsync();
+
+		}
+	}
+
+}
+
+
---
+++ src/EduSim.DataModel.EF/EduSim.DataModel.EF/Interfaces/IEduSimDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace EduSim.Database
+{
+	public interface IApplicationDBContext : IDisposable
+	{
+		//DbSet<Account> AccountList { get; set; }
+		//DbSet<School> SchoolList { get; set; }
+		//DbSet<User> UserList { get; set; }
+		//int SaveChanges();
+		//System.Threading.Tasks.Task<int> SaveChangesAsync();
+		//void MarkAsModified(Account account);
+		//void MarkAsModified(User user);
+	}
+}
+
