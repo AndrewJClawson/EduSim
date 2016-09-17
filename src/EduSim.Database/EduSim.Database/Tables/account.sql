19b66ed410310c1bbe870531d0da396a3faadf16
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-09-17 12:05:45 -0600

    Add sql file for creating accounts table to EduSim.Database project.

---
+++ src/EduSim.Database/EduSim.Database/Tables/account.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `Accounts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `ManagerID` int(11) unsigned DEFAULT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Type` varchar(20) DEFAULT NULL,
+  `CreationDate` date DEFAULT NULL,
+  `CloseDate` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Manager` (`ManagerID`),
+  CONSTRAINT `Manager` FOREIGN KEY (`ManagerID`) REFERENCES `USERS` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
