8172e43a4d295be94884d5d6e2963229b24cf85b
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-09-17 23:20:05 -0600

    add data script to database project.

--- src/EduSim.Database/EduSim.Database/Scripts/initial_develop_data.sql
+++ src/EduSim.Database/EduSim.Database/Scripts/initial_develop_data.sql
@@ -1,2 +1,6 @@
-﻿INSERT INTO accounts (1,1,"EduSim Test Account");
+﻿INSERT INTO accounts VALUES (1,1,"EduSim Test Account");
 INSERT INTO schools VALUES (1,1,"Test School 1","123 Fake St.","My City","CO","80233");
+
+INSERT INTO campuses VALUES (1,1,"First Campus","345 Fake St.","My City","CO","80233");
+
+INSERT INTO users VALUES (1,1,1,1,"Andrew","Clawson","andrewjclawson18@gmail.com","clawson1","T","T","F");
