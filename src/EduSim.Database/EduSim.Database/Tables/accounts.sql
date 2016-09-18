dd4caedb4a2bb2870cb2b71cf2a63756e24fe368
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-09-17 21:24:01 -0600

    Finish account table definition.

--- src/EduSim.Database/EduSim.Database/Tables/accounts.sql
+++ src/EduSim.Database/EduSim.Database/Tables/accounts.sql
@@ -1,1 +1,11 @@
-﻿
+﻿CREATE TABLE IF NOT EXISTS accounts
+(
+	pk_account_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+	fk_account_type_id int(11) unsigned NOT NULL,
+
+	name varchar(20) DEFAULT NULL,
+	
+	PRIMARY KEY (pk_account_id),
+	KEY account_type (fk_account_type_id),
+	CONSTRAINT account_type FOREIGN KEY (fk_account_type_id) REFERENCES edusim._account_types(pk_account_type_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
