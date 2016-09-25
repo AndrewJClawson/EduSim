a7c2883791a060c81bd45739d736d1973ae8ebbe
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-09-25 13:56:52 -0600

    Setup DataRepository

---
+++ src/EduSim/EduSim/DataRepository/Abstractions/IDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSim.DataRepository
+{
+	public interface IDataRepository<TRepoItem> where TRepoItem : class
+	{
+		TRepoItem GetById(object id);
+		IQueryable<TRepoItem> GetAll(params string[] navigationProperties);
+		IQueryable<TRepoItem> FindBy(Expression<Func<TRepoItem, bool>> predicate, params string[] navigationProperties);
+		void Add(TRepoItem item);
+		void Delete(TRepoItem item);
+		void Delete(object id);
+		void Update(TRepoItem item);
+	}
+}
---
+++ src/EduSim/EduSim/DataRepository/DataRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using EduSim.DataModel.EF;
+using EduSim.Models;
+using EduSim.Contexts;
+
+namespace EduSim.DataRepository
+{
+	public class DataRepository<TRepoItem> : IDataRepository<TRepoItem> where TRepoItem : class
+	{
+		protected EduSimContext _edusimContext;
+		private readonly DbSet<TRepoItem> _dbSet;
+		public DataRepository(DbContext dbContext)
+		{
+			_edusimContext = (EduSimContext)dbContext;
+			_dbSet = _edusimContext.Set<TRepoItem>();
+		}
+
+		public TRepoItem GetById(object id)
+		{
+			return _dbSet.Find(id);
+		}
+
+		public IQueryable<TRepoItem> GetAll(params string[] navigationProperties)
+		{
+			IQueryable<TRepoItem> dbQuery = _edusimContext.Set<TRepoItem>();
+			foreach (string navigationProperty in navigationProperties)
+			{
+				dbQuery = dbQuery.Include(navigationProperty);
+			}
+			return dbQuery;
+		}
+
+		public virtual void Add(TRepoItem item)
+		{
+			_dbSet.Add(item);
+			_edusimContext.Entry(item).State = EntityState.Added;
+		}
+
+		public virtual void Delete(object id)
+		{
+			TRepoItem deleteItem = _dbSet.Find(id);
+			//TODO - Check for null object????
+			Delete(deleteItem);
+
+			//TODO - Update state ??
+		}
+
+		public virtual void Delete(TRepoItem item)
+		{
+			// More here check if item state is detached. If it is, attach it.
+			_dbSet.Remove(item);
+			_edusimContext.Entry(item).State = EntityState.Deleted;
+		}
+
+		public virtual void Update(TRepoItem item)
+		{
+			//TODO - null checks on item????
+			_dbSet.Attach(item);
+			_edusimContext.Entry(item).State = EntityState.Modified;
+		}
+	}
+}
