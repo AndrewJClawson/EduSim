f3d6a27984927b9622e38f267a0d060ef15f6778
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-01 16:42:02 -0600

    Refactor EduSim.DataRepository to EduSim.Repository.

--- src/EduSim/EduSim/Repository/Abstractions/IRepository.cs
+++ src/EduSim/EduSim/Repository/Abstractions/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EduSim.DataRepository
+namespace EduSim.Repository
 {
 	public interface IRepository<T> where T : class
 	{
