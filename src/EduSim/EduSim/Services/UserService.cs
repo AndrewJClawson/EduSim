1bd57f827fd6334d36dc2138f12be834c65afcaf
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-02 14:05:55 -0600

    Add user service class.

---
+++ src/EduSim/EduSim/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using EduSim.Models;
+using EduSim.Contexts;
+using EduSim.Repository;
+using System.Data.Entity;
+using System.Data.Linq;
+using MySql.Data.Entity;
+
+namespace EduSim.Services
+{
+	public class UserService
+	{
+		public UserService()
+		{
+		}
+	}
+}
