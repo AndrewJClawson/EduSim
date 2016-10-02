52d09f4cdf2f13e1267da9bee8917b90e8c5d165
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-01 19:11:25 -0600

    Repository class works.

--- src/EduSim.Console/EduSim.Console/Program.cs
+++ src/EduSim.Console/EduSim.Console/Program.cs
@@ -5,24 +5,30 @@ using System.Data.Entity;
 using MySql.Data.Entity;
 using MySql.Data.MySqlClient;
 using EduSim.Models;
-
 using EduSim.Contexts;
+using EduSim.Repository;
 
 namespace EduSim.Console
 {
 	class MainClass
 	{
+		private static Repository<Account> _accountRepository = new Repository<Account>();
+		private static Repository<AccountType> _accountTypeRepository = new Repository<AccountType>();
 		static void Main()
 		{
-			using (EduSimContext context = new EduSimContext())
+			var accountList = _accountRepository.GetAll().ToList();
+			foreach (Account account in accountList)
 			{
-				var accounts = context.Accounts.ToList();
-				foreach (Account account in accounts)
-				{
-					System.Console.WriteLine("Account ID: " + account.AccountId);
-					System.Console.WriteLine("Name: " + account.AccountName);
-				}
+				System.Console.WriteLine(account.AccountName);
 			}
+
+			var accountTypeList = _accountTypeRepository.GetAll();
+			foreach (AccountType type in accountTypeList)
+			{
+				System.Console.WriteLine(type.Name);
+			}
+
+
 
 		}
 	}
