5673b8f464cbdada5b3a6c846e1cc1e6f32ad012
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-08 23:46:39 -0600

    Add user interface and implementation.

---
+++ src/EduSim.Core/ModelInterfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EduSim.Core.ModelInterfaces
+{
+	public interface IUser
+	{
+		int UserId { get; set; }
+		int AccountId { get; set; }
+		int? SchoolId { get; set; }
+		int? CampusId { get; set; }
+		string FirstName { get; set; }
+		string LastName { get; set; }
+		string Email { get; set; }
+		string Password { get; set; }
+		string IsAdmin { get; set; }
+		string IsInstructor { get; set; }
+		string IsVolunteer { get; set; }
+	}
+}
