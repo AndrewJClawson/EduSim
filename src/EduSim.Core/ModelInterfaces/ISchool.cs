15dd9e4f8c056ace37ba67d0c512adf994e75e61
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-09 13:19:38 -0600

    Create school interface and model implementation.

---
+++ src/EduSim.Core/ModelInterfaces/ISchool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EduSim.Core.Models;
+
+namespace EduSim.Core.ModelInterfaces
+{
+	public interface ISchool
+	{
+		int SchoolId { get; set; }
+		int AccountId { get; set; }
+		string SchoolName { get; set; }
+		string Address { get; set; }
+		string City { get; set; }
+		string State { get; set; }
+		string Zip { get; set; }
+	}
+}
---
+++ src/EduSim.Core/Models/School.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+using EduSim.Core.ModelInterfaces;
+
+namespace EduSim.Core.Models
+{
+	[Table("schools")]
+	public class School : ISchool
+	{
+		[Column("pk_school_id")]
+		public int SchoolId { get; set; }
+
+		[Column("fk_account_id")]
+		public int AccountId { get; set; }
+
+		[Column("name")]
+		public string SchoolName { get; set; }
+
+		[Column("address")]
+		public string Address { get; set; }
+
+		[Column("city")]
+		public string City { get; set; }
+
+		[Column("state")]
+		public string State { get; set; }
+
+		[Column("zip")]
+		public string Zip { get; set; }
+
+		public virtual Account Account { get; set; }
+		public virtual ICollection<User> Users { get; set; }
+
+	}
+}
