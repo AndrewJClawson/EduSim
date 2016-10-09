900820ad3baecb4bd667185225bf0f09797303ed
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-08 22:38:07 -0600

    Add account class.

---
+++ src/EduSim.Core/ModelInterfaces/IAccount.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace EduSim.Core.ModelInterfaces
+{
+	public interface IAccount
+	{
+		int AccountId { get; set; }
+		string AccountName { get; set; }
+	}
+}
---
+++ src/EduSim.Core/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using EduSim.Core.ModelInterfaces;
+
+namespace EduSim.Core.Models
+{
+	[Table("accounts")]
+	public class Account : IAccount
+	{
+		[Column("pk_account_id")]
+		public int AccountId { get; set; }
+
+		[Column("fk_account_type_id")]
+		public int AccountTypeId { get; set; }
+
+		[Column("account_name")]
+		public string AccountName { get; set; }
+
+		public Account()
+		{
+		}
+	}
+}
