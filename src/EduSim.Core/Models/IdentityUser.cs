696cc62c3b01663a573942fa0d7976fe1ef4cd4e
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-23 13:00:25 -0600

    Fill out IdentityUser class.

--- src/EduSim.Core/Models/IdentityUser.cs
+++ src/EduSim.Core/Models/IdentityUser.cs
@@ -7,31 +7,35 @@ namespace EduSim.Core.Models
 {
 	public class IdentityUser : IUser
 	{
-		public virtual string Email { get; set; }
+		public IdentityUser()
+		{
+			Id = Guid.NewGuid().ToString();
+		}
 
-		public virtual bool EmailConfirtmed { get; set; }
+		public IdentityUser(string username) : this()
+		{
+			UserName = username;
+		}
 
-		public virtual string Id { get; set;}
+		public string Id {get;set;}
 
-		public virtual bool LockoutEnabled { get; set; }
+		public string UserName {get;set;}
 
-		public virtual DateTime? LockoutEndDateUtc { get; set; }
+		public virtual string Email {get;set;}
 
-		public virtual ICollection<IdentityUserLogin> Logins { get; set; }
+		public virtual bool EmailConfirmed {get;set;}
 
-		public virtual string PasswordHash { get; set; }
+		public virtual string PasswordHash {get;set;}
 
-		public virtual string PhoneNumber { get; set; }
+		public virtual string SecurityStamp {get;set;}
 
-		public virtual bool PhoneNumberConfirmed { get; set; }
+		public virtual string PhoneNumber {get;set;}
 
-		public virtual ICollection<IdentityUserRole> Roles { get; set; }
+		public virtual bool PhoneNumberConfirmed {get;set;}
 
-		public virtual string SecutityStamp { get; set; }
+		public virtual bool TwoFactorEnabled {get;set;}
 
-		public virtual bool TwoFactorEnabled { get; set; }
+		public virtual DateTime? LockoutEndDateUtc {get;set;}
 
-		public virtual string UserName { get; set; }
-
-	}
+		public virtual int AccessFailedCount {get;set;}
 }
