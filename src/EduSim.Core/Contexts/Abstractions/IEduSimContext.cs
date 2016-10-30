9e3458614e9edd8c1f3d0aec211e439e299d7a27
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-10-29 23:07:53 -0600

    Add UnityConfig and register dependency injection containers. Abstract EduSimContext to an interface.

---
+++ src/EduSim.Core/Contexts/Abstractions/IEduSimContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using MySql.Data.Entity;
+using EduSim.Core.Models;
+using EduSim.Core.Models.Identity;
+
+namespace EduSim.Core.Contexts
+{
+	public interface IEduSimContext
+	{
+		DbSet<AccountType> AccountTypes { get; set;}
+		DbSet<Account> Accounts { get; set;}
+		DbSet<User> Users { get; set; }
+		DbSet<School> Schools { get; set; }
+	}
+}
--- src/EduSim.Core/Contexts/EduSimContext.cs
+++ src/EduSim.Core/Contexts/EduSimContext.cs
@@ -10,11 +10,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace EduSim.Core.Contexts
 {
 	[DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
-	public partial class EduSimContext : IdentityDbContext<Microsoft.AspNet.Identity.EntityFramework.IdentityUser>
+	public partial class EduSimContext : IdentityDbContext<Microsoft.AspNet.Identity.EntityFramework.IdentityUser>, IEduSimContext
 	{
 		public DbSet<AccountType> AccountTypes { get; set; }
 		public DbSet<Account> Accounts { get; set; }
-		//public DbSet<IdentityUser> IdentityUserss { get; set; }
+		//public DbSet<IdentityUser> IdentityUsers { get; set; }
+		public DbSet<User> Users { get; set;}
 		public DbSet<School> Schools { get; set; }
 
 		public EduSimContext() : base(nameOrConnectionString:"EduSimContext")
