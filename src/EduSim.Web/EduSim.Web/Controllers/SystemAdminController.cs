a2ace2cef43b1d6d4cd44a1a2d891b2c40bf5d1f
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-19 20:08:19 -0700

    Add system admin controller and role view.

---
+++ src/EduSim.Web/EduSim.Web/Controllers/SystemAdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using EduSim.Core.Models
+using EduSim.Core.ModelInterfaces;
+using EduSim.Web.ViewModels;
+using EduSim.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.Owin.Security
+using Microsoft.Web;
+using System.Threading.Tasks;
+
+
+using System.Web.Mvc;
+
+using EduSim.Core.Services;
+
+namespace EduSim.Web.Controllers
+{
+    public class SystemAdminController : Controller
+    {
+        public ActionResult Roles()
+        {
+            return View ("/Views/SystemAdmin/Roles.cshtml");
+        }
+    }
+}
