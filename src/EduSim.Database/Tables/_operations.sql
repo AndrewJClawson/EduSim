e5b3c39371e58dff329ef7dfa3d41d0b05602558
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-24 21:42:19 -0700

    Add .sql files for content types and operations.

---
+++ src/EduSim.Database/Tables/_operations.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE IF NOT EXISTS _operations
+(
+	pk_operation_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+	name varchar(20) DEFAULT NULL,
+	
+	PRIMARY KEY (pk_operation_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
