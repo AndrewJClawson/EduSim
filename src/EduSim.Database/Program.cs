005c5df75224e2c2e70f90fc48bc83c7a87b5247
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-24 23:04:49 -0700

    Convert EduSim.Database from Class Library to Console App.

---
+++ src/EduSim.Database/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EduSim.Database
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello World! This is the EduSim Database project!");
+		}
+	}
+}
