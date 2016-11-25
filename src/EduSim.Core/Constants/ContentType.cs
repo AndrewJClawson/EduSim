7acbd9239a910a082e2d783d51f57de2133ef346
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-24 22:34:52 -0700

    Add constant classes for ContentType and Operations.

---
+++ src/EduSim.Core/Constants/ContentType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EduSim.Core.Constants
+{
+	public class ContentType
+	{
+		public static Constant Account
+		{
+			get { return new Constant() { Id = 1, Name = "Account" }; }
+		}
+
+		public static Constant User
+		{
+			get { return new Constant() { Id = 2, Name = "User" }; }
+		}
+
+		public static Constant Profile
+		{
+			get { return new Constant() { Id = 3, Name = "Profile" }; }
+		}
+	}
+}
---
+++ src/EduSim.Core/Constants/Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EduSim.Core.Constants
+{
+	public class Operation
+	{
+		public static Constant Create
+		{
+			get { return new Constant() { Id = 1, Name = "Create" };  }
+		}
+
+		public static Constant View
+		{
+			get { return new Constant() { Id = 2, Name = "View" }; }
+		}
+
+		public static Constant Edit
+		{
+			get { return new Constant() { Id = 3, Name = "Edit" }; }
+		}
+
+		public static Constant Delete
+		{
+			get { return new Constant() { Id = 4, Name = "Delete" }; }
+		}
+	}
+}
