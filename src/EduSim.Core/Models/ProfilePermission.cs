cdccb56d78e1f2d2e15d094691284061d18517cb
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-26 12:39:57 -0700

    Update model for profile permissions.

--- src/EduSim.Core/Models/ProfilePermission.cs
+++ src/EduSim.Core/Models/ProfilePermission.cs
@@ -1,12 +1,24 @@
 ﻿using EduSim.Core.ModelInterfaces;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EduSim.Core.Models
 {
-	public class ProfilePermission
+	[Table("profile_permissions")]
+	public class ProfilePermission : IProfilePermission
 	{
-		public ProfilePermission()
-		{
-		}
+		[Column("pk_profile_permission_id")]
+		public int ProfilePermissionId { get; set; }
+
+		[Column("fk_profile_id")]
+		public int ProfileId { get; set; }
+
+		[Column("fk_permission_id")]
+		public int PermissionId { get; set; }
+
+		public virtual Profile Profile { get; set; }
+
+		public virtual Permission Permission { get; set; }
 	}
 }
