adea7a80e2b6d2224e670d56e3e851e3ac1bf8ae
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-26 11:10:16 -0700

    no message

--- src/EduSim.Core/Constants/Permission.cs
+++ src/EduSim.Core/Constants/Permission.cs
@@ -1,10 +1,68 @@
 ﻿using System;
-namespace EduSim.Core
+
+namespace EduSim.Core.Constants
 {
 	public class Permission
 	{
-		public Permission()
+		public static Constant CreateAccounts
 		{
+			get { return new Constant() { Id = 1, Name = "Can create accounts." }; }
+		}
+
+		public static Constant ViewAccounts
+		{
+			get { return new Constant() { Id = 2, Name = "Can view accounts" }; }
+
+		}
+
+		public static Constant EditAccounts
+		{
+			get { return new Constant() { Id = 3, Name = "Can edit accounts." }; }
+		}
+
+		public static Constant DeleteAccounts
+		{
+			get { return new Constant() { Id = 4, Name = "Can delete accounts."};}
+		}
+
+		public static Constant CreateUsers
+		{
+			get { return new Constant() { Id = 5, Name = "Can create users." }; }
+		}
+
+		public static Constant ViewUsers
+		{
+			get { return new Constant() { Id = 6, Name = "Can view users."}; }
+		}
+
+		public static Constant EditUsers
+		{
+			get { return new Constant() { Id = 7, Name = "Can edit users."}; }
+		}
+
+		public static Constant DeleteUsers
+		{
+			get { return new Constant() { Id = 8, Name = "Can delete users."}; }
+		}
+
+		public static Constant CreateProfiles
+		{
+			get { return new Constant() { Id = 9, Name = "Can create profiles." }; }
+		}
+
+		public static Constant ViewProfiles
+		{
+			get { return new Constant() { Id = 10, Name = "Can view profiles." }; }
+		}
+
+		public static Constant EditProfiles
+		{
+			get { return new Constant() { Id = 11, Name = "Can edit profiles."}; }
+		}
+
+		public static Constant DeleteProfiles
+		{
+			get { return new Constant() { Id = 12, Name = "Can delete profiles." }; }
 		}
 	}
 }
--- src/EduSim.Database/ReferenceData/_permissions.sql
+++ src/EduSim.Database/ReferenceData/_permissions.sql
@@ -1,14 +1,14 @@
-﻿INSERT INTO _permissions VALUES (1,1,"Can create an account");
-INSERT INTO _permissions VALUES (2,2,"Can view an account.");
-INSERT INTO _permissions VALUES (3,3,"Can edit an account.");
-INSERT INTO _permissions VALUES (4,4,"Can delete an account.");
+﻿INSERT INTO _permissions VALUES (1,1,"Can create accounts.");
+INSERT INTO _permissions VALUES (2,2,"Can view accounts.");
+INSERT INTO _permissions VALUES (3,3,"Can edit accounts.");
+INSERT INTO _permissions VALUES (4,4,"Can delete accounts.");
 
-INSERT INTO _permissions VALUES (5,5,"Can create a user.");
-INSERT INTO _permissions VALUES (6,6,"Can view a user.");
-INSERT INTO _permissions VALUES (7,7,"Can edit a user.");
-INSERT INTO _permissions VALUES (8,8,"Can delete a user.");
+INSERT INTO _permissions VALUES (5,5,"Can create users.");
+INSERT INTO _permissions VALUES (6,6,"Can view users.");
+INSERT INTO _permissions VALUES (7,7,"Can edit users.");
+INSERT INTO _permissions VALUES (8,8,"Can delete users.");
 
-INSERT INTO _permissions VALUES (9,9,"Can create a profile.");
-INSERT INTO _permissions VALUES (10,10,"Can view a profile.");
-INSERT INTO _permissions VALUES (11,11,"Can edit a profile.");
-INSERT INTO _permissions VALUES (12,12,"Can delete a profile.");
+INSERT INTO _permissions VALUES (9,9,"Can create profiles.");
+INSERT INTO _permissions VALUES (10,10,"Can view profiles.");
+INSERT INTO _permissions VALUES (11,11,"Can edit profiles.");
+INSERT INTO _permissions VALUES (12,12,"Can delete profiles.");
