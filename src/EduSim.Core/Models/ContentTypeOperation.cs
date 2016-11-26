068d55ae7af47d1e34c3c9cc147b05e517fb3125
Andrew Clawson <andrewclawson@Andrews-MBP.domain>
2016-11-26 12:44:34 -0700

    Add data annotations to model for table links.

--- src/EduSim.Core/Models/ContentTypeOperation.cs
+++ src/EduSim.Core/Models/ContentTypeOperation.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 using EduSim.Core.ModelInterfaces;
 
 namespace EduSim.Core.Models
 {
+	[Table("_content_type_operations")]
 	public class ContentTypeOperation : IContentTypeOperation
 	{
+		[Column("pk_content_type_operation_id")]
 		public int ContentTypeOperationId { get; set; }
+
+		[Column("fk_content_type_id")]
 		public int ContentTypeId { get; set; }
+
+		[Column("fk_operation_id")]
 		public int OperationId { get; set; }
 
 		public virtual ContentType ContentType { get; set; }
--- src/EduSim.Core/Models/Permission.cs
+++ src/EduSim.Core/Models/Permission.cs
@@ -1,11 +1,20 @@
 ﻿using EduSim.Core.ModelInterfaces;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EduSim.Core.Models
 {
+	[Table("_permissions")]
 	public class Permission : IPermission
 	{
+		[Column("pk_permission_id")]
 		public int PermissionId { get; set; }
+
+		[Column("fk_content_type_operation_id")]
 		public int ContentTypeOperationId { get; set; }
+
+		[Column("name")]
 		public string Name { get; set; }
 
 		public virtual ContentTypeOperation ContentTypeOperation {get;set;}
