95ed946181e8de953e0ae6ece6cf9477e564d3ad
Andrew Clawson <andrewclawson@Andrews-MacBook-Pro.local>
2017-05-06 13:33:42 -0600

    no message

---
+++ src/EduSim.Web/EduSim.Web/Scripts/SystemAdmin/systemadmin.accounts.js
@@ -0,0 +1,39 @@
+﻿var systemadmin_accounts = (function(){
+
+	function onAppearing() {
+
+	}
+
+	function loadAccountDetails() {
+		var id = $("#SelectedAccountId");
+		var postData = { accountId: id };
+		$.ajax({
+			contentType: "application/json; charset=utf-8",
+			type: "POST",
+			url: "/Account/GetDetails",
+			data: postData,
+			success: function(result) {
+				if (result.Success) {
+					$("#AccountName").val(result.Account.Name);
+				}
+				else {
+					alert("failure loading details");
+				}
+			}
+
+		});
+	}
+
+	$
+	$(document).ready(function(){
+		$("#new-account-button").click(function(){
+			alert("Feature coming soon!");
+		});
+
+		$("#SelectedAccountId").on("change", loadAccountDetails);
+	});
+	return {
+		onAppearing: onAppearing,
+		loadDetails: loadAccountDetails
+	}
+})()
---
+++ src/EduSim.Web/EduSim.Web/ViewModels/AccountDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EduSim.Core.Models;
+
+namespace EduSim.Web.ViewModels
+{
+	public class AccountDetails
+	{
+		public int AccountId { get; set; }
+		public int AccountTypeId { get; set; }
+		public int AccountList { get; set; }
+		public string Name { get; set; }
+
+		public AccountDetails(Account account)
+		{
+			AccountId = account.AccountId;
+			Name = account.AccountName;
+			AccountTypeId = account.AccountTypeId;
+		}
+	}
+}
